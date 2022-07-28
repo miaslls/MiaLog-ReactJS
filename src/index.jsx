8b20b469f097b6e1f83569b99883670c8c1e747d
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-28 20:45:12 -0300

    refactor: ReactDOM.render deprecated

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,8 @@ import Letter from "./components/Letter";
 import Notes from "./components/Notes";
 import Planner from "./components/Planner";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <main>
       <BusinessCard />
@@ -20,6 +21,5 @@ ReactDOM.render(
       <Planner />
       <Notes />
     </main>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
