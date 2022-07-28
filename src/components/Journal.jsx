96fe774b882accfe20b110e3d4f1e019faa81c22
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-28 19:17:29 -0300

    fix: classNameName 🤦‍♀️

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -1,33 +1,33 @@
 function Journal() {
   return (
     <>
-      <section classNameName="element" id="journal-element">
-        <div classNameName="animate-element">
+      <section className="element" id="journal-element">
+        <div className="animate-element">
           <div id="journal-container">
             <div id="journal-body"></div>
             <div id="journal-label">JOURNAL</div>
             <div id="journal-band"></div>
           </div>
           <div id="pencil-container">
-            <div classNameName="pencil-tip" id="tip-wood"></div>
-            <div classNameName="pencil-tip" id="tip-lead"></div>
+            <div className="pencil-tip" id="tip-wood"></div>
+            <div className="pencil-tip" id="tip-lead"></div>
             <div id="pencil-body"></div>
-            <div classNameName="pencil-metal-part"></div>
-            <div classNameName="pencil-metal-part"></div>
-            <div classNameName="pencil-metal-part"></div>
+            <div className="pencil-metal-part"></div>
+            <div className="pencil-metal-part"></div>
+            <div className="pencil-metal-part"></div>
             <div id="pencil-eraser"></div>
           </div>
         </div>
       </section>
 
-      <section classNameName="element-label" id="journal-element-label">
+      <section className="element-label" id="journal-element-label">
         <img
           src="assets/SVG/arrow-journal.svg"
           alt="arrow"
-          classNameName="label-arrow"
+          className="label-arrow"
           id="journal-element-label-arrow"
         />
-        <div classNameName="element-label-text" id="journal-element-label-text">
+        <div className="element-label-text" id="journal-element-label-text">
           <h2>JOURNAL</h2>
           <ul>
             <li>moods</li>
