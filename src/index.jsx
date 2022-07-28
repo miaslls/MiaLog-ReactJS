a6e9b0bbdece8e3cca4403c85286309f77d18442
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-28 19:10:33 -0300

    chore: import components to index

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import BusinessCard from "./components/BusinessCard";
+import Calculator from "./components/Calculator";
+import CreditCards from "./components/CreditCards";
+import Journal from "./components/Journal";
+import Letter from "./components/Letter";
+import Notes from "./components/Notes";
+import Planner from "./components/Planner";
+
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <main>
+      <BusinessCard />
+      <Calculator />
+      <CreditCards />
+      <Journal />
+      <Letter />
+      <Planner />
+      <Notes />
+    </main>
+  </React.StrictMode>,
   document.getElementById("root")
 );
