e71fdb845fa81008e5e1af728a8c7d5b03341750
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-28 19:06:41 -0300

    feat: Notes

---
+++ src/components/Notes.jsx
@@ -0,0 +1,27 @@
+function FunctionName() {
+  return (
+    <>
+      <section class="element" id="notes-element">
+        <div class="animate-element">
+          <div id="notes-container">
+            <div id="notes-title">NOTES</div>
+          </div>
+        </div>
+      </section>
+
+      <section class="element-label" id="notes-element-label">
+        <img
+          src="assets/SVG/arrow-notes.svg"
+          alt="arrow"
+          class="label-arrow"
+          id="notes-element-label-arrow"
+        />
+        <div class="element-label-text" id="notes-element-label-text">
+          <h2>notes</h2>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default FunctionName;
