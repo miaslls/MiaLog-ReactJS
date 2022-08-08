b8895bd3521caa3154f136f247f4904c749d964e
miaslls <97785569+miaslls@users.noreply.github.com>
2022-08-07 22:14:43 -0300

    chore: views (restructure)

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import "./App.css";
-
-import BusinessCard from "./components/BusinessCard/BusinessCard";
-import Calculator from "./components/Calculator/Calculator";
-import CreditCards from "./components/CreditCards/CreditCards";
-import Journal from "./components/Journal/Journal";
-import Letter from "./components/Letter/Letter";
-import Notes from "./components/Notes/Notes";
-import Planner from "./components/Planner/Planner";
+import Home from "./views/Home/Home";
 
 function App() {
   return (
-    <main>
-      <BusinessCard />
-      <Calculator />
-      <CreditCards />
-      <Journal />
-      <Letter />
-      <Planner />
-      <Notes />
-    </main>
+    <>
+      <Home />
+    </>
   );
 }
 
